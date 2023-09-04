9d6f79ebfb1fefaf388a6ffa3b8aaf5414cced74
konateb <111666644+konateb@users.noreply.github.com>
2023-09-04 14:00:39 -0400

    add comment

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
-console.log("Hello World");
-console.log("Hello World");
+
+let x = true;
+console.log("x: ", typeof x);
+const monTableau1 = []; // definir un tableau vide
+//ajouter 5 éléments au tableau
+for (let i = 0; i < 5; i++) {
+  // générer un nombre aléatoire
+  let element = Math.floor(Math.random() * 100);
+  monTableau1.push(element);
+}
+console.log("monTableau1: ", monTableau1);
+const cb = (e) => {
+  return e + e;
+};
+const monTableau2 = monTableau1.map(cb);
+//commenter la ligne suivante
+console.log("monTableau2: ",monTableau2);
+
+
 
